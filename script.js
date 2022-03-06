6474da8f472bd8063bf6e221b037e39a0e9acd27
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-03-06 22:44:38 +0200

    methods

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ class Calculator {
     constructor( previousOperandTextElement, currentOperandTextElement ){
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
+        this.clear()
     }
 
     clear(){
-
+     this.currentOperand = ''
+     this.previousOperand = ''
+     this.operation = undefined
     }
     delete(){
 
@@ -20,7 +23,7 @@ class Calculator {
 
     }
     updateDisplay(){
-        
+
     }
 }
 
@@ -32,3 +35,4 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
