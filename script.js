f20f28d70e14570ed0ad1ab9ff82c7e49827dc41
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-03-06 23:30:17 +0200

    button click

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ class Calculator {
 
     }
     updateDisplay(){
+        
 
     }
 }
@@ -35,4 +36,13 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
 
